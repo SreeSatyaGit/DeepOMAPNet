af6e745ba706216efdca7e550822eff2170ac5f8
bharadwaja2000 <nandivada.s@northeastern.edu>
2025-10-06 02:50:22 -0400

    files update

---
+++ R/RefMapping.R
@@ -0,0 +1,113 @@
+library(Seurat)
+library(SeuratData)
+library(ggplot2)
+library(reticulate)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c(
+  "basilisk", "DelayedArray", "S4Vectors", 
+  "SingleCellExperiment", "SummarizedExperiment"
+))
+BiocManager::install("zellkonverter")
+library(zellkonverter)
+
+# we will use data from 2 technologies for the reference
+#BM.ref <- subset(GSMRNA_Merge, samples %in% c("AML0612", "Control0082"))
+
+with_progress({
+  p <- progressor(10)
+  result <- future.apply::future_lapply(1:10, function(x) {
+    Sys.sleep(1)
+    p()
+    sqrt(x)
+  })
+})
+
+CONTROL$sample <- "CONTROL"
+AMLA$sample    <- "AML"
+
+CONTROL <- RenameCells(CONTROL, add.cell.id = "CTRL")
+AMLA    <- RenameCells(AMLA,    add.cell.id = "AML")
+
+
+
+BM.ref<- bm
+
+#BM.ref[["RNA"]] <- split(BM.ref[["RNA"]], f = BM.ref$samples)
+
+# pre-process dataset (without integration)
+BM.ref <- NormalizeData(BM.ref)
+BM.ref <- FindVariableFeatures(BM.ref)
+BM.ref <- ScaleData(BM.ref)
+BM.ref <- RunPCA(BM.ref)
+BM.ref <- FindNeighbors(BM.ref, dims = 1:30)
+BM.ref <- FindClusters(BM.ref)
+
+BM.ref <- RunUMAP(BM.ref, dims = 1:30)
+DimPlot(BM.ref, group.by = c("Broad_cell_identity", "samples"))
+
+
+BM.ref <- IntegrateLayers(object = BM.ref, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca",
+                        verbose = FALSE)
+
+# re-join layers after integration
+BM.ref[["ADT"]] <- JoinLayers(BM.ref[["ADT"]])
+
+BM.ref <- FindNeighbors(BM.ref, reduction = "integrated.cca", dims = 1:30)
+BM.ref <- FindClusters(BM.ref, resolution = 1)
+
+# Set the path to the directory containing the three files
+data_dir <- "/home/nandivada.s/Datasets/GSM3872434_ETV6-RUNX1_1/"
+cell_anno <- fread("/home/nandivada.s/Datasets/GSE132509_cell_annotations/GSE132509_cell_annotations.tsv.gz")
+
+# Read the data
+etv6_runx1_data <- Read10X(data.dir = data_dir)
+
+# Optionally create a Seurat object directly
+etv6_runx1 <- CreateSeuratObject(counts = etv6_runx1_data, project = "ETV6_RUNX1")
+
+
+# select two technologies for the query datasets
+
+DefaultAssay(adata_gene_test) <- 'ADT'
+BM.query <- adata_gene_test
+BM.query <- NormalizeData(BM.query, normalization.method = 'CLR')
+BM.anchors <- FindTransferAnchors(reference = BM.ref, query = BM.query, dims = 1:30,
+                                        reference.reduction = "pca")
+predictions <- TransferData(anchorset = BM.anchors, refdata = BM.ref$Broad_cell_identity, dims = 1:30)
+
+
+# Add as metadata
+BM.query <- AddMetaData(BM.query, metadata = predictions)
+
+
+BM.ref <- RunUMAP(BM.ref, dims = 1:30, return.model = TRUE)
+BM.query <- MapQuery(anchorset = BM.anchors, reference = BM.ref, query = BM.query,
+                           refdata = list(celltype = "Broad_cell_identity"), reference.reduction = "pca", reduction.model = "umap")
+
+
+
+p1 <- DimPlot(BM.ref, reduction = "umap", group.by = "Broad_cell_identity", label = TRUE, label.size = 3,
+              repel = TRUE) + NoLegend() + ggtitle("Reference annotations")
+p2 <- DimPlot(BM.query, reduction = "ref.umap", group.by = "predicted.id", label = TRUE,
+              label.size = 3, repel = TRUE) + NoLegend() + ggtitle("Query transferred labels")
+p1 + p2
+# If you already removed the extra assay:
+
+
+score_mat <- GetAssayData(BM.query,
+                          assay = "prediction.score.celltype",
+                          slot  = "data")           # or "counts" if preferred
+
+# 2. Transpose so each row = a cell; add a column with the cell ID
+df_scores <- as.data.frame(t(as.matrix(score_mat)))
+df_scores <- cbind(cell_id = rownames(df_scores), df_scores)
+
+# 3. Write to a TSV
+write.table(df_scores,
+            file      = "BM_query_ADT_prediction_scores.tsv",
+            sep       = "\t",
+            row.names = FALSE,
+            quote     = FALSE)
+
+cat("Saved", nrow(df_scores), "cells ×", ncol(df_scores)-1, "cell-type based on Protein scores to BM_query_prediction_scores.tsv\n")
---
+++ R/WNN_Mapping.R
@@ -0,0 +1,126 @@
+library(Seurat)
+library(SeuratData)
+library(cowplot)
+library(dplyr)
+
+adata_gene_train[["ADT"]] <- adata_protein_train[["ADT"]]
+adata_gene_test[["ADT"]] <- adata_protein_test[["ADT"]]
+bm = adata_gene_train
+DefaultAssay(bm) <- 'RNA'
+bm <- NormalizeData(bm) %>% FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+
+
+DefaultAssay(bm) <- 'ADT'
+# we will use all ADT features for dimensional reduction
+# we set a dimensional reduction name to avoid overwriting the 
+VariableFeatures(bm) <- rownames(bm[["ADT"]])
+bm <- NormalizeData(bm, normalization.method = 'CLR', margin = 2) %>% 
+  ScaleData() %>% RunPCA(reduction.name = 'apca')
+
+# Identify multimodal neighbors. These will be stored in the neighbors slot, 
+# and can be accessed using bm[['weighted.nn']]
+# The WNN graph can be accessed at bm[["wknn"]], 
+# and the SNN graph used for clustering at bm[["wsnn"]]
+# Cell-specific modality weights can be accessed at bm$RNA.weight
+bm <- FindMultiModalNeighbors(
+  bm, reduction.list = list("pca", "apca"), 
+  dims.list = list(1:30, 1:18), modality.weight.name = "RNA.weight"
+)
+
+bm <- RunUMAP(bm, nn.name = "weighted.nn", reduction.name = "wnn.umap", reduction.key = "wnnUMAP_")
+bm <- FindClusters(bm, graph.name = "wsnn", algorithm = 3, resolution = 2, verbose = FALSE)
+
+p1 <- DimPlot(bm, reduction = 'wnn.umap', label = TRUE, repel = TRUE, label.size = 2.5) + NoLegend()
+p2 <- DimPlot(bm, reduction = 'wnn.umap', group.by = 'Broad_cell_identity', label = TRUE, repel = TRUE, label.size = 2.5) + NoLegend()
+p1 + p2
+
+bm <- RunUMAP(bm, reduction = 'pca', dims = 1:30, assay = 'RNA', 
+              reduction.name = 'rna.umap', reduction.key = 'rnaUMAP_')
+bm <- RunUMAP(bm, reduction = 'apca', dims = 1:18, assay = 'ADT', 
+              reduction.name = 'adt.umap', reduction.key = 'adtUMAP_')
+
+p3 <- DimPlot(bm, reduction = 'rna.umap', group.by = 'Broad_cell_identity', label = TRUE, 
+              repel = TRUE, label.size = 2.5) + NoLegend()
+p4 <- DimPlot(bm, reduction = 'adt.umap', group.by = 'Broad_cell_identity', label = TRUE, 
+              repel = TRUE, label.size = 2.5) + NoLegend()
+p3 + p4
+
+DefaultAssay(bm) <- 'RNA'
+bm <- RunSPCA(bm, assay = 'RNA',graph = 'wsnn')
+
+bm <- FindNeighbors(
+  object = bm,
+  reduction = "spca",
+  dims = 1:30,
+  graph.name = "spca.annoy.neighbors", 
+  k.param = 50,
+  cache.index = TRUE,
+  return.neighbor = TRUE,
+  l2.norm = TRUE
+)
+
+
+DefaultAssay(adata_gene_test) <- 'RNA'
+BM.query <- adata_gene_test
+BM.query <- NormalizeData(BM.query)
+BM.query<- FindVariableFeatures(BM.query)
+BM.query <- ScaleData(BM.query, assay = 'RNA')
+
+
+anchors <- list()
+
+  anchors <- FindTransferAnchors(
+    reference = bm,
+    query = BM.query,
+    k.filter = NA,
+    reference.reduction = "spca", 
+    reference.neighbors = "spca.annoy.neighbors", 
+    dims = 1:30
+  )
+  predictions <- TransferData(anchorset = anchors, refdata = bm$Broad_cell_identity, dims = 1:30)
+  BM.query <- AddMetaData(BM.query, metadata = predictions)
+  
+  
+  bm <- RunUMAP(bm, dims = 1:30, return.model = TRUE)
+
+  BM.query <- tryCatch({
+    MapQuery(
+      anchorset = anchors, 
+      query = BM.query,
+      reference = bm, 
+      refdata = list(celltype = "Broad_cell_identity"),
+      reference.reduction = "spca",
+      reduction.model = "umap"
+    )
+  }, error = function(e) {
+    message(paste(e$message))
+    return(NULL)  # or return the original object: BM.query.batches[[i]]
+  })
+
+  p1 <- DimPlot(bm, reduction = "wnn.umap", group.by = "Broad_cell_identity", label = TRUE, label.size = 3,
+                repel = TRUE) + NoLegend() + ggtitle("Reference annotations")
+  p2 <- DimPlot(BM.query, reduction = "ref.spca", group.by = "predicted.id", label = TRUE,
+                label.size = 3, repel = TRUE) + NoLegend() + ggtitle("Query transferred labels SPCA")
+  p3 <- DimPlot(BM.query, reduction = "ref.umap", group.by = "predicted.id", label = TRUE,
+                label.size = 3, repel = TRUE) + NoLegend() + ggtitle("Query transferred labels UMAP")
+  p1 + p2 + p3
+  # If you already removed the extra assay:
+  
+  
+  score_mat <- GetAssayData(BM.query,
+                            assay = "prediction.score.celltype",
+                            slot  = "data")           # or "counts" if preferred
+  
+  # 2. Transpose so each row = a cell; add a column with the cell ID
+  df_scores <- as.data.frame(t(as.matrix(score_mat)))
+  df_scores <- cbind(cell_id = rownames(df_scores), df_scores)
+  
+  # 3. Write to a TSV
+  write.table(df_scores,
+              file      = "BM_query_ADT_prediction_spca_scores.tsv",
+              sep       = "\t",
+              row.names = FALSE,
+              quote     = FALSE)
+  
+  cat("Saved", nrow(df_scores), "cells ×", ncol(df_scores)-1, "cell-type based on Protein scores to BM_query_prediction_spca_scores.tsv\n")
+  
